c2e5fa7e89734e07e877b2887f3c2b22f1208f33
Jose alberto Gonzalez rouille <spin.jag@gmail.com>
2016-07-21 23:52:04 -0500

    crud groups and users

---
+++ app/Controller/GroupsController.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Groups Controller
+ *
+ * @property Group $Group
+ * @property PaginatorComponent $Paginator
+ */
+class GroupsController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+}
---
+++ app/Model/User.php
@@ -0,0 +1,67 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ * @property Group $Group
+ */
+class User extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'code' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				'message' => 'No puede ser nulo',
+				'allowEmpty' => false,
+			)
+		),
+		'group_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				'message' => 'Debe pertenecer a un grupo',
+				'allowEmpty' => false,
+			),
+		),
+		'name' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				'message' => 'Escriba un nombre',
+				'allowEmpty' => false,
+			),
+		),
+		'zipcode' => array(
+			'postal' => array(
+				'rule' => array('postal'),
+				'message' => 'Codigo postal valido',
+				'allowEmpty' => true,
+				'required' => false,
+			),
+		),
+	);
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Group' => array(
+			'className' => 'Group',
+			'foreignKey' => 'group_id',
+		)
+	);
+}
